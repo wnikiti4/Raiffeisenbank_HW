7e71cefc7ae05a54687026073af12f42bdedadd4
wnikiti4 <wnikiti4@gmail.com>
2020-11-24 17:44:54 +0300

    Done task 12

--- src/test/java/test/HW_2_8.java
+++ src/test/java/test/HW_2_8.java
@@ -7,6 +7,8 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.opera.OperaDriver;
+import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.util.ArrayList;
 import java.util.Collections;
@@ -23,43 +25,51 @@ public class HW_2_8 {
 
     @Test
     public void Test_1() {
-        String maskName = "b";
-        String maskCode = "00002";
-        String phone = '+'+ maskCode + maskCode;
-        String email = maskName+'@'+maskName+'.'+maskName;
-
+        String maskText = "aaaaa";
+        String maskCode = "0001";
+        WebDriverWait wait= new WebDriverWait(opDriver,10);
  //       loginToAdmin();
-        String href = "http://localhost/litecart/en/create_account";
-        opDriver.navigate().to(href);
-       opDriver.findElement(By.cssSelector("input[name=\"firstname\"]")).sendKeys(maskName);
-        opDriver.findElement(By.cssSelector("input[name=\"lastname\"]")).sendKeys(maskName);
-        opDriver.findElement(By.cssSelector("input[name=\"address1\"]")).sendKeys(maskName);
-        opDriver.findElement(By.cssSelector("input[name=\"postcode\"]")).sendKeys(maskCode);
-        opDriver.findElement(By.cssSelector("input[name=\"city\"]")).sendKeys(maskCode);
-        opDriver.findElement(By.cssSelector(".select2-selection__arrow")).click();
-        opDriver.findElement(By.cssSelector("li[id*=\"US\"]")).click();
-        opDriver.findElement(By.cssSelector("input[name=\"email\"]")).sendKeys(email);
-        opDriver.findElement(By.cssSelector("input[name=\"phone\"]")).sendKeys(phone);
-        opDriver.findElement(By.cssSelector("input[name=\"password\"]")).sendKeys(phone);
-        opDriver.findElement(By.cssSelector("input[name=\"confirmed_password\"]")).sendKeys(phone);
-        opDriver.findElement(By.cssSelector("[name=\"create_account\"]")).click();
-        System.out.println("Пользователь зарегестрирован");
-        opDriver.findElement(By.cssSelector("a[href=\"http://localhost/litecart/en/logout\"]")).click();
-        System.out.println("Вышли");
-        opDriver.findElement(By.cssSelector("[name=\"email\"]")).sendKeys(email);
-        opDriver.findElement(By.cssSelector("[name=\"password\"]")).sendKeys(phone);
-        opDriver.findElement(By.cssSelector("[name=\"login\"]")).click();
-        System.out.println("Зашили");
-        opDriver.findElement(By.cssSelector("a[href=\"http://localhost/litecart/en/logout\"]")).click();
-        System.out.println("Вышли");
-        opDriver.close();
-        System.out.println("Закрытие браузера");
-    }
-
-    @Test
-    public void Test_2(){
+        String href = "http://localhost/litecart/admin/?app=catalog&doc=catalog";
         loginToAdmin();
-
+        opDriver.navigate().to(href);
+        int Amount =opDriver.findElements(By.cssSelector("tr.row")).size();
+        WebElement button = wait.until((WebDriver d) -> d.findElement(By.cssSelector(".button:nth-of-type(2)")));
+        button.click();
+        opDriver.findElement(By.cssSelector("[value=\"1\"]")).click();
+        opDriver.findElement(By.cssSelector("[name = \"name[en]\"]")).sendKeys(maskText);
+        opDriver.findElement(By.cssSelector("[name = \"code\"]")).sendKeys(maskCode);
+        opDriver.findElement(By.cssSelector("[value = \"1-3\"]")).click();
+        opDriver.findElement(By.cssSelector("[name = \"quantity\"]")).sendKeys("1");
+        String path = System.getProperty("user.dir");
+        opDriver.findElement(By.cssSelector("[name = \"new_images[]\"]")).sendKeys(path+"\\src\\test\\java\\test\\Resources\\Im.jpg");
+        opDriver.findElement(By.cssSelector("[name = \"date_valid_from\"]")).sendKeys("20111990");
+        opDriver.findElement(By.cssSelector("[name = \"date_valid_to\"]")).sendKeys("20112022");
+        System.out.println("General заполнена");
+        opDriver.findElement(By.cssSelector("[href = \"#tab-information\"]")).click();
+        Select select = new Select(opDriver.findElement(By.cssSelector("[name = \"manufacturer_id\"]")));
+        select.selectByIndex(1);
+        opDriver.findElement(By.cssSelector("[name = \"keywords\"]")).sendKeys(maskText);
+        opDriver.findElement(By.cssSelector("[name = \"short_description[en]\"]")).sendKeys(maskText);
+        opDriver.findElement(By.cssSelector("[name = \"description[en]\"]")).sendKeys(maskText+maskText);
+        opDriver.findElement(By.cssSelector("[name = \"head_title[en]\"]")).sendKeys(maskText);
+        opDriver.findElement(By.cssSelector("[name = \"meta_description[en]\"]")).sendKeys(maskText);
+        opDriver.findElement(By.cssSelector("[href=\"#tab-prices\"]")).click();
+        opDriver.findElement(By.cssSelector("[name=\"purchase_price\"]")).sendKeys("1");
+        Select select1 = new Select(opDriver.findElement(By.cssSelector("[name = \"purchase_price_currency_code\"]")));
+        select1.selectByValue("USD");
+        opDriver.findElement(By.cssSelector("[name = \"gross_prices[USD]\"")).sendKeys("1");
+        opDriver.findElement(By.cssSelector("[name = \"gross_prices[EUR]\"")).sendKeys("1");
+        opDriver.findElement(By.cssSelector("[name=\"save\"]")).click();
+        // Не очень понял какую именно проверку вы хотите, так что сделал 2
+        if(areElementsPresent(opDriver,By.cssSelector("[href*=\"id=7\"]"))){
+            if(Amount < opDriver.findElements(By.cssSelector("tr.row")).size()){
+                System.out.println("Test done");
+            }
+        }else {
+            System.out.println("Чет пошло не так");
+            new failTest("Товар не смог сохраниться");
+        }
+        opDriver.close();
     }
 
 
