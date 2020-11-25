7e10706e3aa253eab5e507fd23be727169912f3c
wnikiti4 <wnikiti4@gmail.com>
2020-11-26 01:32:41 +0300

    Done task 14

--- src/test/java/test/HW_2_8.java
+++ src/test/java/test/HW_2_8.java
@@ -7,15 +7,13 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.opera.OperaDriver;
-import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
-import static org.openqa.selenium.support.ui.ExpectedConditions.attributeToBe;
-import static org.openqa.selenium.support.ui.ExpectedConditions.numberOfElementsToBeLessThan;
+import static org.openqa.selenium.support.ui.ExpectedConditions.numberOfWindowsToBe;
+
 
 public class HW_2_8 {
     private OperaDriver opDriver;
@@ -29,39 +27,35 @@ public class HW_2_8 {
     @Test
     public void Test_1() {
         WebDriverWait wait = new WebDriverWait(opDriver, 10);
-        for (int i = 1; i <= 3; i++) {
-            opDriver.navigate().to("http://Localhost/litecart");
-            opDriver.findElements(By.cssSelector(".product")).get(0).click();
-            if (areElementsPresent(opDriver, By.cssSelector("[name=\"options[Size]\"]"))) {
-                Select select = new Select(opDriver.findElements(By.cssSelector("[name=\"options[Size]\"]")).get(0));
-                select.selectByIndex(1);
-            }
-            opDriver.findElement(By.cssSelector("[name=\"add_cart_product\"]")).click();
-            if (wait.until(attributeToBe(opDriver.findElement(By.cssSelector("span.quantity")),
-                    "textContent",
-                    Integer.toString(i)))) {
-                System.out.println("Товар добавлен");
-            } else {
-                System.out.println("Чет не добавилось");
-                opDriver.close();
-            }
-        }
-        opDriver.findElement(By.cssSelector("a.link[href=\"http://localhost/litecart/en/checkout\"]")).click();
-        int size = opDriver.findElements(By.cssSelector("td.item")).size();
-        for (int j = size; j >= 1; j--) {
-            opDriver.findElement(By.cssSelector("[name=\"remove_cart_item\"]")).click();
-            // мне не нравиться как это выглядит, есть ли более элегнтный способ ?
-            if (wait.until(numberOfElementsToBeLessThan(By.cssSelector("td.item"), j)).size() != -1) {
-                System.out.println("Товар удален");
-            } else {
-                System.out.println("Все сломалось");
-                opDriver.close();
-            }
+        loginToAdmin();
+        opDriver.navigate().to("http://localhost/litecart/admin/?app=countries&doc=countries");
+        opDriver.findElement(By.cssSelector("td[style=\"text-align: right;\"] a[href*=\"AF\"]")).click();
+        for(int i = 0; i < opDriver.findElements(By.cssSelector("form  [target=\"_blank\"]")).size();i++){
+            String mainWindow = opDriver.getWindowHandle();
+            Set<String> oldWindows = opDriver.getWindowHandles();
+            opDriver.findElements(By.cssSelector("form  [target=\"_blank\"]")).get(i).click();
+            wait.until(numberOfWindowsToBe(2));
+            Set<String> newWindows = opDriver.getWindowHandles();
+            // Получили XOR по 2 коллекциям
+            newWindows.removeAll(oldWindows);
+            // Получили ID новой страници
+            String idWindowNew = newWindows.iterator().next();
+            opDriver.switchTo().window(idWindowNew);
+            opDriver.close();
+            opDriver.switchTo().window(mainWindow);
         }
         opDriver.close();
     }
 
-
+    public ExpectedCondition<String> anyWindowOtherThan(Set<String> oldWindows) {
+        return new ExpectedCondition<String>() {
+            public String apply(WebDriver driver) {
+                Set<String> handles = driver.getWindowHandles();
+                handles.removeAll(oldWindows);
+                return handles.size() > 0 ? handles.iterator().next() : null;
+            }
+        };
+    }
     private void sortCheck(String href, String s, String attribute) {
         opDriver.navigate().to(href);
         List<WebElement> listCountry = opDriver.findElements(By.cssSelector(s));
