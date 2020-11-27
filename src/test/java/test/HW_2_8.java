36c07fc2a0bcc20c596ad4d7a629a100507d7f06
wnikiti4 <wnikiti4@gmail.com>
2020-11-27 18:43:28 +0300

    Done task 17

--- src/test/java/test/HW_2_8.java
+++ src/test/java/test/HW_2_8.java
@@ -4,77 +4,61 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
-import org.openqa.selenium.Capabilities;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.logging.LogEntry;
 import org.openqa.selenium.opera.OperaDriver;
-import org.openqa.selenium.opera.OperaOptions;
-import org.openqa.selenium.remote.DesiredCapabilities;
-import org.openqa.selenium.remote.RemoteWebDriver;
-import org.openqa.selenium.support.events.AbstractWebDriverEventListener;
-import org.openqa.selenium.support.events.EventFiringWebDriver;
-import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
-import static org.openqa.selenium.support.ui.ExpectedConditions.numberOfWindowsToBe;
-
+import static org.openqa.selenium.support.ui.ExpectedConditions.presenceOfElementLocated;
 
 public class HW_2_8 {
-    private EventFiringWebDriver opDriver;
+    private OperaDriver opDriver;
+    private List<WebElement> listEl;
+    private List<WebElement> listProd;
 
     @Before
     public void start() {
-
-        opDriver = new EventFiringWebDriver(new OperaDriver());
-        opDriver.register(new MyListener());
+        opDriver = new OperaDriver();
 
     }
-
-    public static class MyListener extends AbstractWebDriverEventListener {
-        @Override
-        public void beforeFindBy(By by, WebElement element, WebDriver driver) {
-            System.out.println(by);
-        }
-
-        @Override
-        public void afterFindBy(By by, WebElement element, WebDriver driver) {
-            System.out.println(by +  " found");
-        }
-
-        @Override
-        public void onException(Throwable throwable, WebDriver driver) {
-            System.out.println(throwable);
-        }
-    }
-
     @Test
-    public void Test_1() throws MalformedURLException {
-        DesiredCapabilities caps = new DesiredCapabilities();
-
-        caps.setCapability("os", "Windows");
-        caps.setCapability("os_version", "7");
-        caps.setCapability("browser", "Chrome");
-        caps.setCapability("browser_version", "79");
-        caps.setCapability("name", "bsuser750215555631's First Test");
-
-        WebDriver driver = new RemoteWebDriver(new URL(URL), caps);
-        opDriver.navigate().to("https://www.google.ru");
-        driver.quit();
-    }
-
-    public ExpectedCondition<String> anyWindowOtherThan(Set<String> oldWindows) {
-        return new ExpectedCondition<String>() {
-            public String apply(WebDriver driver) {
-                Set<String> handles = driver.getWindowHandles();
-                handles.removeAll(oldWindows);
-                return handles.size() > 0 ? handles.iterator().next() : null;
+    public void Test_1() {
+        loginToAdmin();
+        WebDriverWait wait = new WebDriverWait(opDriver, 10);
+        opDriver.navigate().to("http://localhost/litecart/admin/?app=catalog&doc=catalog&category_id=0");
+        wait.until(presenceOfElementLocated(By.cssSelector(".fa-folder-open")));
+        int j = 0;
+        int  passedElements = 0;
+        while (areElementsPresent(opDriver, By.cssSelector(".fa.fa-folder"))) {
+            listEl = opDriver.findElements(By.cssSelector(" i.fa.fa-folder + a"));
+            String currentHref = listEl.get(0).getAttribute("href");
+            listEl.get(0).click();
+            int size = opDriver.findElements(By.cssSelector("tr.row td:nth-of-type(3) [href *= \"product\"]")).size();
+            while ( j  != (size-passedElements)){
+                listProd = opDriver.findElements(By.cssSelector("tr.row td:nth-of-type(3) [href *= \"product\"]"));
+                listProd.get(j).click();
+                j++;
+                CheckLogs(opDriver);
+                wait.until(presenceOfElementLocated(By.cssSelector("td#content")));
+                opDriver.navigate().to(currentHref);
             }
-        };
+            passedElements = j;
+            j = 0;
+        }
+        opDriver.close();
     }
+
+    private void CheckLogs(WebDriver driver) {
+        for (LogEntry l : driver.manage().logs().get("browser").getAll()) {
+            System.out.println(l);
+        }
+    }
+
     private void sortCheck(String href, String s, String attribute) {
         opDriver.navigate().to(href);
         List<WebElement> listCountry = opDriver.findElements(By.cssSelector(s));
@@ -93,10 +77,6 @@ public class HW_2_8 {
 
     }
 
-    boolean listCheck(List<String> oneList, List<String> twoList) {
-        return true;
-    }
-
     static class failTest extends Exception {
         public failTest(String message) {
             super(message);
@@ -105,7 +85,6 @@ public class HW_2_8 {
 
     void loginToAdmin() {
         opDriver.navigate().to(" http://localhost/litecart/admin/");
-        System.out.println("Открытие браузера");
         opDriver.findElement(By.name("username")).sendKeys("admin");
         opDriver.findElement(By.name("password")).sendKeys("admin");
         opDriver.findElement(By.name("login")).click();
@@ -122,3 +101,6 @@ public class HW_2_8 {
     }
 
 }
+/*1) зайти в админку
+2) открыть каталог, категорию, которая содержит товары (страница http://localhost/litecart/admin/?app=catalog&doc=catalog&category_id=1)
+3) последовательно открывать страницы товаров и проверять, не появляются ли в логе браузера сообщения (любого уровня)*/
